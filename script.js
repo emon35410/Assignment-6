cfa7a2fecbcc3752791b15b630de98f44cba2c6a
emon35410 <mhhsan341@gmail.com>
2025-09-08 13:51:24 +0600

    modal adding done

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+
+
+
 const loadButton = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
         .then(res => res.json())
         .then((json) => displayButton(json.categories))
+
+        loadAllButton()
 }
 
+const loadTreeDetails = (id)=>{
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(details=>displayLoadTreeDetails(details.plants))
+}
+const displayLoadTreeDetails =(detail=>{
+    const detailsContainer = document.getElementById("details-container")
+    detailsContainer.innerHTML=`
+       <div class="space-x-4">
+            <h1 class="font-bold">${detail.name}</h1>
+            <img class="max-w-[400px] max-h-[300px] w-full h-auto rounded-lg" src="${detail.image}" alt="">
+            <h2><span class="font-bold ">Category:</span> ${detail.category}</h2>
+            <h3><span class="font-bold">Price:</span> ৳${detail.price}</h3>
+            <p><span class="font-bold">Description:</span> ${detail.description}</p>
+        </div>
+    `
+    document.getElementById("my_modal_5").showModal();
+})
 const loadAllButton =()=>{
     fetch("https://openapi.programming-hero.com/api/plants")
     .then(res=>res.json())
@@ -18,7 +42,7 @@ const displayAllButton =(plant)=>{
             <div class="tree-card bg-white shadow-sm rounded-lg p-4 space-y-3">
                             <img class="bg-[#EDEDED] rounded-lg w-full h-40 object-cover" src="${card.image}"
                                 alt="">
-                            <h2 class="text-2xl font-bold">${card.name}</h2>
+                            <h2 onclick="loadTreeDetails(${card.id})" class="text-2xl font-bold hover:cursor-pointer">${card.name}</h2>
                             <p class="text-[#1f293763] text-sm">${card.description}</p>
                             <div class="flex justify-between items-center">
                                 <button class="bg-[#DCFCE7] px-3 py-1 text-sm text-green-800 rounded-full">${card.category}</button>
@@ -34,12 +58,20 @@ const displayAllButton =(plant)=>{
 }
 
 
-
+const removeActive = ()=>{
+    const activeBtn = document.querySelectorAll(".tree-class")
+    activeBtn.forEach((btn)=>btn.classList.remove("active"))
+}
 const loadLevelCard = (id) => {
     const url = (`https://openapi.programming-hero.com/api/category/${id}`)
     fetch(url)
         .then(res => res.json())
-        .then(data => displayCard(data.plants))
+        .then(data => {
+            removeActive();
+            const clickBtn = document.getElementById(`btn-${id}`)
+            clickBtn.classList.add("active")
+            displayCard(data.plants)
+        })
 }
 const displayCard = (plants) => {
     const cardContainer = document.getElementById("card-container")
@@ -50,7 +82,7 @@ const displayCard = (plants) => {
     <div class="tree-card bg-white shadow-sm rounded-lg p-4 space-y-3">
                             <img class="bg-[#EDEDED] rounded-lg w-full h-40 object-cover" src="${plant.image}"
                                 alt="">
-                            <h2 class="text-2xl font-bold">${plant.name}</h2>
+                            <h2 onclick="loadTreeDetails(${plant.id})" class="text-2xl font-bold hover:cursor-pointer">${plant.name}</h2>
                             <p class="text-[#1f293763] text-sm">${plant.description}</p>
                             <div class="flex justify-between items-center">
                                 <button class="bg-[#DCFCE7] px-3 py-1 text-sm text-green-800 rounded-full">${plant.category}</button>
@@ -65,13 +97,16 @@ const displayCard = (plants) => {
     })
 }
 
+
+
+
 const displayButton = (categories) => {
     const buttonContainer = document.getElementById("button-container")
     buttonContainer.innerHTML = ""
     categories.forEach(categorie => {
         const btnDiv = document.createElement("div")
         btnDiv.innerHTML = `
-        <button onclick="loadLevelCard(${categorie.id})" class="bg-green-700 text-white px-4 my-3 py-1 w-50 text-start rounded-lg hover:cursor-pointer hover:bg-red-300">
+        <button id="btn-${categorie.id}" onclick="loadLevelCard(${categorie.id})" class="bg-green-700 text-white px-4 my-3 py-1 w-50 text-start rounded-lg hover:cursor-pointer hover:bg-blue-600 tree-class ">
                                 ${categorie.category_name}
                             </button>
         `
